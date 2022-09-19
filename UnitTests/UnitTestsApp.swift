bf1e7924c89d308641aa681976c2ad2dcfbc676c
Anthony José <anthonyJ2017@icloud.com>
2022-09-19 11:03:17 -0300

    update

---
+++ UnitTests/UnitTestsApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitTestsApp.swift
+//  UnitTests
+//
+//  Created by Anthony José on 18/09/22.
+//
+
+import SwiftUI
+
+@main
+struct UnitTestsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
